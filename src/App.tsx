096f48c99fed3ec7ed79226a3ed94dbe8d4b8d23
Joe Balan - Git <joebalan7@gmail.com>
2021-02-19 01:10:27 +0530

    Login form with label + fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <form method="post">
+        <div className="container">
+          <label htmlFor="uname"><b>Email</b></label>
+          <input type="text" placeholder="Enter Email" name="uname" required />
+
+          <label htmlFor="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="psw" required />
+
+          <div className="text-center">
+            <button type="submit">Login</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
